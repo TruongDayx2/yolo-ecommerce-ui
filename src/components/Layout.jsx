7a18a12cd0a49808e3097a7da0cfd5abb5e4f78e
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-09-13 17:58:05 +0700

    heroSlide reposive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children }) => {
     return (
-        <div className="body">
+        <div className="main">
             <div className="container">{children}</div>
         </div>
     );
